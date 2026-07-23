3675b8342587aada97275dafcfce9b96d0d2105a
stikk9112233 <sunnyplayz59@gmail.com>
2026-07-23 13:38:37 +0530

    Create run.py

---
+++ run.py
@@ -0,0 +1,11 @@
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(
+        "backend.app.main:app",
+        host="0.0.0.0",
+        port=port,
+        reload=False
+    )
