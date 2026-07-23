947ca95949effc75451f78f23f0cbd60b1f6364d
stikk9112233 <sunnyplayz59@gmail.com>
2026-07-23 22:20:02 +0530

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import { useContext, useEffect, useState } from 'react';
-import { AuthContext } from '../context/auth';
+import { AuthContext } from '../context/AuthProvider';
 
 export default function Home() {
   const auth = useContext(AuthContext) || {};
