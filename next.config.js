5087274ecb0490849bc96baad9b75f8047abb83f
stikk9112233 <sunnyplayz59@gmail.com>
2026-07-24 00:51:02 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+  staticPageGenerationTimeout: 120,
+  trailingSlash: false,
+  basePath: '',
+  assetPrefix: '',
+};
+
+module.exports = nextConfig;
