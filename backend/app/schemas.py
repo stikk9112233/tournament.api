49ae408f9f785eee0c045b2e2406e6213b03e164
stikk9112233 <sunnyplayz59@gmail.com>
2026-07-24 18:04:30 +0530

    Update schemas.py

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -11,6 +11,10 @@ class UserBase(BaseModel):
 class UserCreate(UserBase):
     password: str
 
+class UserLogin(BaseModel):
+    email: str
+    password: str
+
 class UserResponse(UserBase):
     id: int
     created_at: datetime
