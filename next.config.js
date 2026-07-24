10e0abf4b7762285ab8a370a4ec5073ccf9cd52d
stikk9112233 <sunnyplayz59@gmail.com>
2026-07-24 09:34:31 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,28 +5,7 @@ const nextConfig = {
   staticPageGenerationTimeout: 120,
   trailingSlash: false,
   basePath: '',
-  assetPrefix: '',
-  headers: async () => {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=3600, s-maxage=3600',
-          },
-        ],
-      },
-    ];
-  },
-  rewrites: async () => {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://tournamentapi-production-e303.up.railway.app/api/:path*',
-      },
-    ];
-  },
+  assetPrefix: ''
 };
 
 module.exports = nextConfig;
