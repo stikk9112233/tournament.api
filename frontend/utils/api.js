af6047559f778c0eccf085a2b05d7a1f8a2caa80
stikk9112233 <sunnyplayz59@gmail.com>
2026-07-25 15:31:27 +0530

    Update api.js

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
+const const API_URL = 'https://tournament-backend-991a.onrender.com';
 
 const api = axios.create({
   baseURL: `${API_URL}/api`,
